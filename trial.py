4cc10d137a75b53a3b04ad6b7fbc583c2cd4f1ae
John Paul <johnpaul@trees.local>
2021-10-22 12:17:00 +0300

    3/4 way through credentials class

--- trial.py
+++ trial.py
@@ -1,8 +1,34 @@
-# class credentials:
-#   '''
-#  class that holds credentials commands and functions
+class Credentials:
+    '''
+      class that holds credentials commands and functions
+   '''
 
-# '''
+    credential_list = []
+
+    def __init__(self, account, username, password):
+        self.account = account
+        self.username = username
+        self.password -= password
+
+    def delete_account(self):
+        Credentials.credential_list.remove(self)
+
+    def add_account(self):
+        Credentials.credential_list.append(self)
+
+    @classmethod
+    def find_account(cls, account):
+        for credential in cls.credential_list:
+            if credential.account == account:
+                return account
+
+    def find_account_exist(cls, account):
+        for credential in cls.credential_list:
+            if credential.account == account:
+                return True
+        return False
+
+
 class User:
     '''
     class that holds user functions.
@@ -23,3 +49,10 @@ class User:
         Function that saves new user to the userlist array
         """
         User.user_list.append(self)
+
+    @classmethod
+    def delete_user(self):
+        User.user_list.remove(self)
+
+    def displays_users(cls):
+        return cls.user_list
